bf79b1bf488964c60aa400e8a4dafcdb29ab204b
Maja <majacvetkovic@outlook.com>
2021-04-17 21:11:43 +0200

    slider

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,48 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Header = () => {
-  return <header className="header"></header>;
+  var settings = {
+    lazyLoad: true,
+    accessibility: true,
+    arrows: true,
+    slidesToShow: 1,
+    swipe: true,
+    useCss: true,
+    responsive: [
+      {
+        breakpoint: 481,
+        settings: {
+          centerPadding: "0",
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          centerPadding: "0",
+        },
+      },
+    ],
+  };
+
+  return (
+    <header className="header">
+      <Slider {...settings}>
+        <div className="header__slide header__slide--slide1">
+          <h3>caption</h3>
+        </div>
+        <div className="header__slide header__slide--slide2">
+          <h3>2</h3>
+        </div>
+        <div className="header__slide header__slide--slide3">
+          <h3>3</h3>
+        </div>
+      </Slider>
+    </header>
+  );
 };
 
 export default Header;
