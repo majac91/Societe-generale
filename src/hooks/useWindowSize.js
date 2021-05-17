ef10c349ca544c6212ae8b498b02a9396e4cc40f
Maja <majacvetkovic@outlook.com>
2021-05-17 12:32:43 +0200

    clean up useWindowSize

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,13 +1,9 @@
 import { useState, useEffect, useRef } from "react";
 
 export const useWindowSize = () => {
-  // const isClient = typeof window === "object"; //Object represents browser window
   const lastWidth = useRef();
 
   function getSize() {
-    // return {
-    //   width: isClient ? window.innerWidth : undefined,
-    // };
     return {
       width: window.innerWidth,
     };
@@ -16,10 +12,6 @@ export const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState(getSize);
 
   useEffect(() => {
-    // if (!isClient) {
-    //   return false;
-    // }
-
     function handleResize() {
       if (window?.innerWidth !== lastWidth.current) {
         const width = getSize();
